ef5734020bf92ff54ea60f189e77c65f5b944ee6
Rustem <redzhik@gmail.com>
2020-06-10 22:29:38 +0300

    Созданы табы, создан item

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         speed: 1000,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
